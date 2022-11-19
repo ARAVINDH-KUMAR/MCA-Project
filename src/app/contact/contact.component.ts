1fb5902d489058d569d6e20575e3f6975144ecd4
Vichu3010 <45705524+Vichu3010@users.noreply.github.com>
2022-11-19 22:21:49 +0530

    Component Update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,6 +11,14 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target as HTMLFormElement, 'YOUR_PUBLIC_KEY')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error: { text: any; }) => {
+        console.log(error.text);
+      });
+  }
 
 }
